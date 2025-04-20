b93bcb7d96707c386c8d10172dad3e3b3672b974
webqueretaro <arcosqueretaro@gmail.com>
2025-04-20 10:54:50 -0600

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,8 @@
-const CACHE_NAME = 'pwa-queretaro-v15';
+const CACHE_NAME = 'pwa-queretaro-v16';
 const urlsToCache = [
   '/paw/',
   '/paw/index.html',
   '/paw/offers.html',
-  '/paw/styles.css',
-  '/paw/manifest.json',
-  '/paw/icons/icon-512.png',
   '/paw/ofertas1.png',
   '/paw/ofertas2.png',
   '/paw/ofertas3.png',
@@ -14,50 +11,32 @@ const urlsToCache = [
   '/paw/ofertas6.png',
   '/paw/ofertas7.png',
   '/paw/ofertas8.png',
-  '/paw/ofertas9.png'
+  '/paw/ofertas9.png',
+  '/paw/manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache abierto:', CACHE_NAME);
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            console.log('Eliminando cache antiguo:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
       );
-    }).then(() => {
-      console.log('Service Worker activado, forzando actualización');
-      self.clients.claim();
-    })
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
-});
-
-self.addEventListener('message', event => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
 });
